a2e133294d62c8f154d0d1d2c4ad84739cfe90b8
Guilherme Felipe do Nascimento Reis <gui@predictchurn.io>
2019-10-04 19:36:34 +0100

    Virtual Schema for Spot || adding Cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const user = ''; 
 const password = ''; 
@@ -14,6 +15,7 @@ mongoose.connect(connectString, {
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
